67466cb3c2e888946102123c264f84de798a0340
Miguel Oliveira <migueloliveira1794@gmail.com>
2016-10-22 12:29:47 +0100

    moved comics fetch to asynctask and added fetching and noConnection UI

---
+++ app/src/main/java/com/migueloliveira/androidsample/view/CharacterDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.migueloliveira.androidsample.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.migueloliveira.androidsample.R;
+
+public class CharacterDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_details);
+    }
+}
--- app/src/main/java/com/migueloliveira/androidsample/view/fragments/ComicFragment.java
+++ app/src/main/java/com/migueloliveira/androidsample/view/fragments/ComicFragment.java
@@ -2,6 +2,7 @@ package com.migueloliveira.androidsample.view.fragments;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -20,16 +23,21 @@ import com.migueloliveira.androidsample.network.MarvelAPI;
 import com.migueloliveira.androidsample.network.ServiceGenerator;
 import com.migueloliveira.androidsample.view.adapters.ComicRecyclerViewAdapter;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ComicFragment extends Fragment {
-    private static  final String KEY_LAYOUT_MANAGER = "layoutManager";
     private OnComicFragmentInteractionListener mListener;
-    private static final MarvelAPI mMarvelAPI = ServiceGenerator.createService(MarvelAPI.class);
+
+    private RecyclerView mRecyclerView;
+    private ImageView mNoConnectionIv;
+    private ProgressBar mProgressBar;
 
     public ComicFragment() {
     }
@@ -48,47 +56,75 @@ public class ComicFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_comic, container, false);
 
-        final RecyclerView recyclerView = (RecyclerView) view;
-        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressbar);
+        mProgressBar.setIndeterminate(true);
 
-        Call<JsonObject> getComics = mMarvelAPI.getComics(100,0);
-        getComics.enqueue(new Callback<JsonObject>() {
+        mNoConnectionIv = (ImageView) view.findViewById(R.id.noconnection);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mRecyclerView.getContext()));
+
+        new AsyncTask<Void,Void,List<Comic>>() {
+            final MarvelAPI mMarvelAPI = ServiceGenerator.createService(MarvelAPI.class);
+
             @Override
-            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+            protected List<Comic> doInBackground(Void... params) {
                 ArrayList<Comic> comicArrayList = new ArrayList<>();
-                if (response.isSuccessful()) {
-                    JsonObject data = response.body().getAsJsonObject("data");
-                    JsonArray results = data.getAsJsonArray("results");
-                    for (int i = 0; i < results.size(); i++) {
-                        JsonObject comic = results.get(i).getAsJsonObject();
-                        Integer cId = comic.get("id").getAsInt();
-                        String cName = comic.get("title").getAsString();
-                        String cDescription = comic.get("variantDescription").getAsString();
-                        String cThumbnail = comic.get("thumbnail").getAsJsonObject().get("path").getAsString();
-                        String cExtension = comic.get("thumbnail").getAsJsonObject().get("extension").getAsString();
-                        Comic fComic = new Comic(cId, cName, cDescription, cThumbnail, cExtension);
-                        comicArrayList.add(fComic);
+                Call<JsonObject> getComics = mMarvelAPI.getComics(100,0);
+                try {
+                    Response<JsonObject> response = getComics.execute();
+                    if (response.isSuccessful()) {
+                        JsonObject data = response.body().getAsJsonObject("data");
+                        JsonArray results = data.getAsJsonArray("results");
+                        for (int i = 0; i < results.size(); i++) {
+                            JsonObject comic = results.get(i).getAsJsonObject();
+                            Integer cId = comic.get("id").getAsInt();
+                            String cName = comic.get("title").getAsString();
+                            String cDescription = comic.get("variantDescription").getAsString();
+                            String cThumbnail = comic.get("thumbnail").getAsJsonObject().get("path").getAsString();
+                            String cExtension = comic.get("thumbnail").getAsJsonObject().get("extension").getAsString();
+                            Comic fComic = new Comic(cId, cName, cDescription, cThumbnail, cExtension);
+                            comicArrayList.add(fComic);
+                        }
+                        return comicArrayList;
                     }
-                    ComicRecyclerViewAdapter adapter = new ComicRecyclerViewAdapter(comicArrayList, new OnComicInteractionListener() {
-                        @Override
-                        public void onClick(Comic comic) {
-                            Log.e("_DEBUG_",comic.toString());
-                        }
-
-                        @Override
-                        public void onLongPress(Comic mComic) {
-
-                        }
-                    });
-                    recyclerView.setAdapter(adapter);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    if (e instanceof UnknownHostException) {
+                        return null;
+                    }
                 }
+
+                return comicArrayList;
             }
 
             @Override
-            public void onFailure(Call<JsonObject> call, Throwable t) {
+            protected void onPostExecute(List<Comic> comics) {
+                mProgressBar.setVisibility(View.GONE);
+                if (comics == null) {
+                    mNoConnectionIv.setVisibility(View.VISIBLE);
+                } else {
+                    if (comics.size() == 0) {
+                        //TODO show emptyLayout;
+                    } else {
+                        ComicRecyclerViewAdapter adapter = new ComicRecyclerViewAdapter(comics, new OnComicInteractionListener() {
+                            @Override
+                            public void onClick(Comic comic) {
+                                Log.e("_DEBUG_",comic.toString());
+                            }
+
+                            @Override
+                            public void onLongPress(Comic mComic) {
+
+                            }
+                        });
+                        mRecyclerView.setAdapter(adapter);
+                    }
+                }
 
             }
-        });
+        }.execute();
+
         return view;
     }
 
@@ -98,6 +134,7 @@ public class ComicFragment extends Fragment {
         super.onAttach(context);
         if (context instanceof OnComicFragmentInteractionListener) {
             mListener = (OnComicFragmentInteractionListener) context;
+            mListener.onComicFragmentShow();
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnComicFragmentInteractionListener");
